4dd03d6fc4255000c028b77136217418c599faa8
= <=>
2024-04-02 00:41:05 +0700

    3rd commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("ssss");
+const express = require('express');
+const app = express();
+const PORT = 1234;
+app.use(express.urlencoded());
+
+//TO DO ROUTING
+
+app.listen(PORT, ()=>{
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
